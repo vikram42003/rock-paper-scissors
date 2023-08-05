4f78ae3b2fc0683a2f627b9e83bc99f94c3bd89b
Vikram <vikramjit360@gmail.com>
2023-08-06 00:16:25 +0530

    finish 6/6 functions. Error in 'getPlayerChoice' unresolved as of yet

--- javascript.js
+++ javascript.js
@@ -11,6 +11,11 @@
 "use strict";
 
 let gameCounter = Number(prompt("How many rounds of the game do you want to play", ""))
+playRockPaperScissors(gameCounter);
+
+
+
+
 
 function playRockPaperScissors() {
   let computerScore = 0;
@@ -21,7 +26,7 @@ function playRockPaperScissors() {
     let playerChoice = getPlayerChoice();
 
     let roundWinner = compareChoices(computerChoice, playerChoice);
-    displyResult();
+    displyResult(roundWinner);
 
     roundWinner ? playerScore++ : computerScore++;
 
@@ -31,9 +36,6 @@ function playRockPaperScissors() {
 
   displayFinalResult(computerScore, playerScore);
 }
-
-
-
 
 function getComputerChoice() {
   let Choice = Math.floor(Math.random() * 3);
@@ -49,8 +51,39 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-  let Choice = prompt("Rock, Paper or Scissors?").toLowerCase();
+  do {
+    let Choice = prompt("Rock, Paper or Scissors?").toLowerCase();
+    if (Choice != "rock" || Choice != "paper" || Choice != "scissors") {
+      alert("Invalid option or a typo. Please choose between Rock, Paper and Scissors (case insensitive).")
+      Choice = "wrong";
+    }
+  } while (Choice === "wrong")
+  
   return Choice;
+}
+
+function compareChoices(computerChoice, playerChoice) {
+  if (playerChoice === "rock" && computerChoice === "scissors") {
+    return 1;
+  }
+  else if (playerChoice === "paper" && computerChoice === "rock") {
+    return 1;
+  }
+  else if (playerChoice === "scissors" && computerChoice === "paper") {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+function displayResult(roundWinner) {
+  if (roundWinner === 1) {
+    console.log("Player Wins!");
+  }
+  else {
+    console.log("Computer Wins!");
+  }
 }
 
 function displayFinalResult(computerScore, playerScore) {
